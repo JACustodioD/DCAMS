b69e83131ca44be329e00094503d46d481973b38
Marisol Mondragón <marisol9430@hotmail.com>
2019-10-19 20:01:40 -0500

    Encabezado de menu, se agrego el logo y texto de menu

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Nunito|Open+Sans&display=swap" rel="stylesheet"> 
     <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/hover.css" media="all">
     <link rel="stylesheet" href="/css/estilos.css">
@@ -22,12 +22,32 @@
                 <button class="btn btn-primary"><i class="fas fa-user mr-2"></i>Iniciar sesión</button>
             </div>
         </div>
+
+        <!--SOCIAL RED-->
         <div class="row mt-2">
             <div class="col-md-12 d-flex justify-content-end">
                 <a href=""><i class="fab fa-instagram  icon-head hvr-grow-rotate"></i> </a>  
                 <a href=""><i class="fab fa-facebook-square icon-head hvr-grow-rotate"></i></a>
                 <a href=""><i class="fab fa-twitter-square icon-head hvr-grow-rotate"></i> </a>   
             </div>
+        </div>
+
+        <!--LOGOTIPO-->
+        <div class="row">
+            <div class="col-md-2">
+                <img src="/img/icono-dental.png" alt="" class="icono">
+            </div>
+
+         <!--MENU-->
+            <div class="col-md-10">
+                <ul class="d-flex justify-content-around align-items-center menu">
+                    <a href=""><li class="hvr-underline-from-left">Home</li></a>
+                    <a href=""><li class="hvr-underline-from-left">Acerca de</li></a> 
+                    <a href=""><li class="hvr-underline-from-left">Tratamientos</li></a> 
+                    <a href=""><li class="hvr-underline-from-left">Promociones</li></a> 
+                    <a href=""><li class="hvr-underline-from-left">Contacto</li></a>           
+                </ul>
+             </div>
         </div>
     </header>
     <script src="/js/bootstrap.min.js"></script>
