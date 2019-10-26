aec387b3fd37351f7e432297a4ccb33ecdcfbcfa
Marisol Mondragón <marisol9430@hotmail.com>
2019-10-25 22:43:47 -0500

    trabajando el area de promociones con imagenes y seccion de contacto con mapa y texto

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -153,7 +153,7 @@
                      <p class="text-center p-3 parrafo-about">Nuestra <b>misión</b> es brindar un servicio odontológico integral de calidad,
                         con una atención personalizada y en un ambiente agradable; poniendo a su disposición 
                         nuestros servicios especializados comprometiendonos con el bienestar y satisfacción de nuestros pacientes.</p>
-                        <hr class="separator">
+                        <hr id="separator">
                  </div>
              </div>
              <div class="row">
@@ -194,7 +194,7 @@
         <div class="tratamientos-section">
             <div class="row">
                 <div class="col-md-12 mt-3" id="tratamiento1">
-                    <h1 class="pl-4 tratamientos">Tratamientos para ti</h1>
+                    <h1 class="pl-4">Tratamientos para ti.</h1>
                 </div>
             </div>
             <div class="row pl-4">
@@ -224,6 +224,7 @@
             </div>
         </div>
      </section>
+     <!--GIF-->
      <section class="container">
         <div class="gif-implante">
             <div class="row">
@@ -233,11 +234,59 @@
             </div>
         </div>
      </section>
+     <!--PROMOCIONES-->
      <section class="container">
         <div class="promociones">
             <div class="row">
                 <div class="col-md-12 mt-3">
-                    <h1 class="pl-4">Nuestras promociones</h1>
+                    <h1 class="pl-4 text-section"> Promociones. </h1>
+                    <span class="pl-4 text-section">Disfruta de las promociones que Amy tiene para ti</span>
+                </div>
+            </div>
+            <div class="row mt-3">
+              <div class="col-md-4">
+                <img src="/img/image-promo.jpg" class="pl-3 pb-3" alt="" height="300px" width="330px">
+              </div>
+              <div class="col-md-4">
+                <img src="/img/image-promo1.jpg" class="pl-3 pb-3" alt="" height="300px" width="330px"> 
+             </div>
+             <div class="col-md-4">
+                <img src="/img/image-promo2.jpg" class="pl-3 pb-3" alt="" height="300px" width="330px"> 
+             </div>
+           </div>
+        </div>
+     </section>
+     <section class="container">
+        <div class="contacto">
+            <div class="row mt-3">
+                <div class="col-md-6">
+                     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d939.9968264891002!2d-99.15708017083129!3d19.542153799176397!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f79a7764e5e3%3A0x32a6f5e67a308dfa!2sAv%20Alfredo%20del%20Mazo%20V%C3%A9lez%2029%2C%20Chalma%20La%20Barranca%2C%2054143%20Tlalnepantla%20de%20Baz%2C%20M%C3%A9x.!5e0!3m2!1sen!2smx!4v1572051999050!5m2!1sen!2smx"
+                          width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="">
+                     </iframe>
+                </div>
+                <div class="col-md-6">
+                    <h1>Contáctenos</h1>
+                    <span><i class="fas fa-map-marker-alt icon-contacto"></i>Av Alfredo del Mazo Vélez 29, Chalma La Barranca,
+                         54143 Tlalnepantla de Baz, Méx.
+                    </span>
+                    <hr id="separator-contacto">
+                    <span><i class="fas fa-phone icon-contacto"></i>
+                        5522-9901 ext 29
+                   </span>
+                   <hr id="separator-contacto">
+                   <span> <i class="fas fa-clock icon-contacto"></i>
+                    Horarios: Lunes a Viernes 8:00 a 17:00
+                   </span>
+                   <hr id="separator-contacto">
+                   <span>
+                        <a href=""><i class="fab fa-instagram  icon-contacto hvr-grow-rotate"></i> </a>  
+                        <a href=""><i class="fab fa-facebook-square icon-contacto hvr-grow-rotate"></i></a>
+                        <a href=""><i class="fab fa-twitter-square icon-contacto hvr-grow-rotate"></i> </a> 
+                   </span>
+                   <hr id="separator-contacto">
+                   <span>
+                       <button class="btn btn-primary">Agendar una cita</button>
+                   </span>
                 </div>
             </div>
         </div>
